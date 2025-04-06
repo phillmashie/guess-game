24d6faee1535acbbb51c272052f2ceb7c3ce831a
Phillip Mashingaidze <phillmashie@gmail.com>
2025-04-06 20:33:13 +0200

    Allowing Multiple Guesses with Looping

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,19 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    loop {
+        println!("Please input your guess.");
 
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
